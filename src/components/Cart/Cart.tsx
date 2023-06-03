89b15fd02bf0d6bc0f85d750aeeb92d537c3d7db
Nicanev <nic7ven@gmail.com>
2023-06-03 23:45:30 +0400

    optimization

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -7,203 +7,243 @@ import {Loader} from "../UI/Loader/Loader";
 import {Link} from "react-router-dom";
 
 export function Cart() {
-    const [cartData, setCartData] = useState<any>([]);
-    const [cost, setCost] = useState(0);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState<any>(null);
+  const [cartData, setCartData] = useState<any>([]);
+  const [cost, setCost] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState<any>(null);
 
-    const incrementHandler = async (productId: any) => {
-        const token = localStorage.getItem("token");
-        const user = parseJwt(localStorage.getItem("token"));
-        try {
-            const response = await axios.put(
-                `${config.apiUrl}/cart/product`,
-                {
-                    user_id: user.id,
-                    product_id: productId,
-                    count: 1,
-                },
-                {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
+  const incrementHandler = async (productId: any) => {
+    const token = localStorage.getItem("token");
+    const user = parseJwt(localStorage.getItem("token"));
+    try {
+      const response = await axios.put(
+        `${config.apiUrl}/cart/product`,
+        {
+          user_id: user.id,
+          product_id: productId,
+          count: 1,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const updatedCartData = cartData.map((item: any) => {
+        if (item.id === productId) {
+          return {
+            ...item,
+            CartProducts: {
+              ...item.CartProducts,
+              count: item.CartProducts.count + 1,
+            },
+          };
+        }
+        return item;
+      });
+      setCartData(updatedCartData);
+      calculateCost(updatedCartData); // Update the cost
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const decrementHandler = async (productId: any, count: any) => {
+    if (count === 1) {
+      return;
+    }
+    const token = localStorage.getItem("token");
+    const user = parseJwt(localStorage.getItem("token"));
+    try {
+      const response = await axios.put(
+        `${config.apiUrl}/cart/product`,
+        {
+          user_id: user.id,
+          product_id: productId,
+          count: -1,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      const updatedCartData = cartData.map((item: any) => {
+        if (item.id === productId) {
+          return {
+            ...item,
+            CartProducts: {
+              ...item.CartProducts,
+              count: item.CartProducts.count - 1,
+            },
+          };
+        }
+        return item;
+      });
+      setCartData(updatedCartData);
+      calculateCost(updatedCartData); // Update the cost
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteFromCart = async (productId: any, count: any) => {
+    const token = localStorage.getItem("token");
+    const user = parseJwt(localStorage.getItem("token"));
+    try {
+      const response = await axios.delete(`${config.apiUrl}/cart/product`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        data: {
+          user_id: user.id,
+          product_id: productId,
+          count: count,
+        },
+      });
+      fetchCart();
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const fetchCart = async () => {
+    setLoading(true);
+    setError(null);
+    const token = localStorage.getItem("token");
+    const user = parseJwt(localStorage.getItem("token"));
+    try {
+      const response = await axios.get(`${config.apiUrl}/cart/${user.id}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const products = response.data.products;
+      setCartData(products);
+      calculateCost(products); // Update the cost
+      console.log(response.data.products);
+
+      products.forEach((product: any) => {
+        axios
+          .get(`${config.apiUrl}/product-img/product/${product.id}`)
+          .then((response) => {
+            const imageUrl = response.data[0].url;
+            setCartData((prevProducts: any) =>
+              prevProducts.map((prevProduct: any) => {
+                if (prevProduct.id === product.id) {
+                  return {
+                    ...prevProduct,
+                    image: imageUrl,
+                  };
                 }
+                return prevProduct;
+              })
             );
-            fetchCart();
-            console.log(response.data);
-        } catch (error) {
-            console.error(error);
-        }
-    };
+          })
+          .catch((error) => {
+            console.log(error.message);
+          });
+      });
+    } catch (error) {
+      setError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
 
-    const decrementHandler = async (productId: any, count: any) => {
-        if (count === 1) {
-            return;
-        }
-        const token = localStorage.getItem("token");
-        const user = parseJwt(localStorage.getItem("token"));
-        try {
-            const response = await axios.put(
-                `${config.apiUrl}/cart/product`,
-                {
-                    user_id: user.id,
-                    product_id: productId,
-                    count: -1,
-                },
-                {
-                    headers: {
-                        Authorization: `Bearer ${token}`,
-                    },
-                }
-            );
-            fetchCart();
-            console.log(response.data);
-        } catch (error) {
-            console.error(error);
-        }
-    };
-
-    const deleteFromCart = async (productId: any, count: any) => {
-        const token = localStorage.getItem("token");
-        const user = parseJwt(localStorage.getItem("token"));
-        try {
-            const response = await axios.delete(`${config.apiUrl}/cart/product`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-                data: {
-                    user_id: user.id,
-                    product_id: productId,
-                    count: count,
-                },
-            });
-            fetchCart();
-            console.log(response.data);
-        } catch (error) {
-            console.error(error);
-        }
-    };
-    const fetchCart = async () => {
-        setLoading(true);
-        setError(null);
-        const token = localStorage.getItem("token");
-        const user = parseJwt(localStorage.getItem("token"));
-        try {
-            const response = await axios.get(`${config.apiUrl}/cart/${user.id}`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-            const products = response.data.products;
-            setCartData(products);
-            setCost(response.data.total_cost);
-            console.log(response.data.products);
-
-            products.forEach((product: any) => {
-                axios
-                    .get(`${config.apiUrl}/product-img/product/${product.id}`)
-                    .then((response) => {
-                        const imageUrl = response.data[0].url;
-                        setCartData((prevProducts: any) =>
-                            prevProducts.map((prevProduct: any) => {
-                                if (prevProduct.id === product.id) {
-                                    return {
-                                        ...prevProduct,
-                                        image: imageUrl,
-                                    };
-                                }
-                                return prevProduct;
-                            })
-                        );
-                    })
-                    .catch((error) => {
-                        console.log(error.message);
-                    });
-            });
-        } catch (error) {
-            setError(error);
-        } finally {
-            setLoading(false);
-        }
-    };
-
-    useEffect(() => {
-
-
-        fetchCart();
-    }, []);
-    return (
-        <>
-            <div className="cart">
-                <div className="cart__container">
-                    <h1>Корзина</h1>
-                    {loading ? (
-                        <Loader/>
-                    ) : error ? (
-                        <p>Error: {error.message}</p>
-                    ) : (
-                        <div className="cart__table">
-                            <ul className="cart__list">
-                                {cartData.length > 0 ? (
-                                    cartData.map((item: any) => (
-                                        <li key={item.id}>
-                                            <img
-                                                src={item.image}
-                                                alt="Product"
-                                                className="cart__img"
-                                            />
-                                            <div className="cart__name">
-                                                <span>Название:</span>
-                                                {item.name}
-                                            </div>
-                                            <div className="cart__count">
-                                                <span>Кол-во:</span>
-                                                <div className="cart__count-input">
-                                                    <button
-                                                        onClick={() =>
-                                                            decrementHandler(item.id, item.CartProducts.count)
-                                                        }
-                                                    >
-                                                        -
-                                                    </button>
-                                                    <span>{item.CartProducts.count}</span>
-                                                    <button type={"button"} onClick={() => incrementHandler(item.id)}>
-                                                        +
-                                                    </button>
-                                                </div>
-                                            </div>
-                                            <div className="cart__price">
-                                                <span>Цена:</span>{" "}
-                                                {(
-                                                    item.total_price * item.CartProducts.count
-                                                ).toLocaleString()}
-                                            </div>
-                                            <div
-                                                className="cart__delete"
-                                                onClick={() =>
-                                                    deleteFromCart(item.id, item.CartProducts.count)
-                                                }
-                                            ></div>
-                                        </li>
-                                    ))
-                                ) : (
-                                    <li>Корзина пуста</li>
-                                )}
-
-                                {cartData.length > 0 && (
-                                    <li>
-                                        <Link to="/payment">
-                                            <button className="cart__buy">Оплатить</button>
-                                        </Link>
-                                        <span>Сумма: {cost.toLocaleString()}</span>
-                                    </li>
-                                )}
-                            </ul>
-                            <div className="cart__end"></div>
-                        </div>
-                    )}
-                </div>
-            </div>
-        </>
+  const calculateCost = (cartItems: any[]) => {
+    const totalCost = cartItems.reduce(
+      (acc, item) => acc + item.total_price * item.CartProducts.count,
+      0
     );
+    setCost(totalCost);
+  };
+
+  useEffect(() => {
+    fetchCart();
+  }, []);
+
+  return (
+    <>
+      <div className="cart">
+        <div className="cart__container">
+          <h1>Корзина</h1>
+          {loading ? (
+            <Loader />
+          ) : error ? (
+            <p>Error: {error.message}</p>
+          ) : (
+            <div className="cart__table">
+              <ul className="cart__list">
+                {cartData.length > 0 ? (
+                  cartData.map((item: any) => (
+                    <li key={item.id}>
+                      <img
+                        src={item.image}
+                        alt="Product"
+                        className="cart__img"
+                      />
+                      <div className="cart__name">
+                        <span>Название:</span>
+                        {item.name}
+                      </div>
+                      <div className="cart__count">
+                        <span>Кол-во:</span>
+                        <div className="cart__count-input">
+                          <button
+                            onClick={() =>
+                              decrementHandler(
+                                item.id,
+                                item.CartProducts.count
+                              )
+                            }
+                          >
+                            -
+                          </button>
+                          <span>{item.CartProducts.count}</span>
+                          <button
+                            type={"button"}
+                            onClick={() => incrementHandler(item.id)}
+                          >
+                            +
+                          </button>
+                        </div>
+                      </div>
+                      <div className="cart__price">
+                        <span>Цена:</span>{" "}
+                        {(
+                          item.total_price * item.CartProducts.count
+                        ).toLocaleString()}
+                      </div>
+                      <div
+                        className="cart__delete"
+                        onClick={() =>
+                          deleteFromCart(item.id, item.CartProducts.count)
+                        }
+                      ></div>
+                    </li>
+                  ))
+                ) : (
+                  <li>Корзина пуста</li>
+                )}
+
+                {cartData.length > 0 && (
+                  <li>
+                    <Link to="/payment">
+                      <button className="cart__buy">Оплатить</button>
+                    </Link>
+                    <span>Сумма: {cost.toLocaleString()}</span>
+                  </li>
+                )}
+              </ul>
+              <div className="cart__end"></div>
+            </div>
+          )}
+        </div>
+      </div>
+    </>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -3,18 +3,14 @@ import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-import { Provider } from "react-redux";
-import { store } from "./store";
-import "./firebase";
+
 
 const root = ReactDOM.createRoot(
 	document.getElementById("root") as HTMLElement
 );
 root.render(
 	<BrowserRouter>
-		<Provider store={store}>
 			<App />
-		</Provider>
 	</BrowserRouter>
 );
 
